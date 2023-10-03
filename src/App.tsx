157d79e19d41d9caf9427511f1a83fcd7642f6e3
AgusNazer <agustin.nazer@hotmail.com>
2023-10-03 19:27:13 -0300

    probando

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  // const [subs, setSubs] = useState<string | number>([
+  //   {
+  //     nick: "pepe",
+  //     submonths: 3,
+  //     avatar: "",
+  //     description: "q seyo ni idea",
+  //   },
+  //   {
+  //     nick: "juan",
+  //     submonths: 32,
+  //     avatar: "",
+  //     description: "sfgfdgdgfdfg",
+  //   }
+  // ]);
+
+  return 
+  
+  <div className="app">
+
+
+
+  </div>;
+}
+
+export default App;
